e0131e39fd3f6fb945b5dc67446a87d8d0b3bf94
adamrais <adamrais14@gmail.com>
2021-09-08 11:25:58 -0400

    Initial Commit

---
+++ HydrationTracker/HydrationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationTrackerApp.swift
+//  HydrationTracker
+//
+//  Created by Adam Rais on 2021-09-08.
+//
+
+import SwiftUI
+
+@main
+struct HydrationTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
